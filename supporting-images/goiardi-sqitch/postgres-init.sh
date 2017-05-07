3c4996087b3e7aa7155c07480bc4a4806385c14a
ctdk <jbingham@gmail.com>
2017-05-06 17:38:13 -0700

    tweak the postgres init script

--- supporting-images/goiardi-sqitch/postgres-init.sh
+++ supporting-images/goiardi-sqitch/postgres-init.sh
@@ -5,11 +5,20 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-createuser -h $POSTGRES_HOST -U $POSTGRES_USER $GOIARDI_USER
-createdb -h $POSTGRES_HOST -U $POSTGRES_USER -O $GOIARDI_USER $GOIARDI_DB
+until psql -U $POSTGRES_USER -h $POSTGRES_HOST -c '\l'; do
+	>&2 echo "postgres still starting...."
+	sleep 1
+done
+
+>&2 echo "postgres up - initializing database"
+
+createuser -h $GOIARDI_POSTGRESQL_HOST -U $POSTGRES_USER $GOIARDI_POSTGRESQL_USERNAME
+createdb -h $GOIARDI_POSTGRESQL_HOST -U $POSTGRES_USER -O $GOIARDI_POSTGRESQL_USERNAME $GOIARDI_DB
 
 cd /src/goiardi-schema/postgres
 
-sqitch deploy db:pg://$POSTGRES_USER:@$POSTGRES_HOST/$GOIARDI_DB
+sqitch deploy db:pg://$POSTGRES_USER:@$GOIARDI_POSTGRESQL_HOST/$GOIARDI_POSTGRESQL_DBNAME
 
-sed 's/_GOIARDI_DB_/'$GOIARDI_DB'/g; s/_GOIARDI_USER_/'$GOIARDI_USER'/g;' < /supporting-files/alter-postgres.sql | psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $GOIARDI_DB
+sed 's/_GOIARDI_DB_/'$GOIARDI_POSTGRESQL_DBNAME'/g; s/_GOIARDI_USER_/'$GOIARDI_POSTGRESQL_USERNAME'/g;' < /supporting-files/alter-postgres.sql | psql -U $POSTGRES_USER -h $GOIARDI_POSTGRESQL_HOST -d $GOIARDI_POSTGRESQL_DBNAME
+
+>&2 echo "finished initializing goiardi database"
